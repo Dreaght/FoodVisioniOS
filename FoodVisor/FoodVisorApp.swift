6e062927558c685a0f5dfb216637a7329ecaed7b
dreaght <dreaght@icloud.com>
2025-02-16 15:01:34 +0300

    Initial Commit

---
+++ FoodVisor/FoodVisorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodVisorApp.swift
+//  FoodVisor
+//
+//  Created by Nikita Efimov on 2/16/25.
+//
+
+import SwiftUI
+
+@main
+struct FoodVisorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
