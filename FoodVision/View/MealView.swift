aaf64a89fc304225839e41d7a5fd3ba34bfa483a
dreaght <dreaght@icloud.com>
2025-03-01 21:48:25 +0300

    diary ui

---
+++ FoodVision/View/MealView.swift
@@ -0,0 +1,18 @@
+//
+//  MealView.swift
+//  FoodVision
+//
+//  Created by Nikita Efimov on 3/1/25.
+//
+
+import SwiftUI
+
+struct MealView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MealView()
+}
