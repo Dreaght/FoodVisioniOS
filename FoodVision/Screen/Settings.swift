569a49375c8d0951bd273e262af8c5b3e7c00c16
Tracy Chan <tracysychan@outlook.com>
2025-03-12 00:22:22 +0800

    feat: scroll value picker v2

--- FoodVision/Screen/Settings.swift
+++ FoodVision/Screen/Settings.swift
@@ -1,11 +1,16 @@
 import SwiftUI
 
 struct Settings: View {
+    @State var currWeight = 50
+    @State private var showPicker: Bool = false // Control visibility of the picker
     var body: some View {
         VStack {
-            Text("Settings")
-                .font(.title)
-                .padding()
+            Form {
+                Section("About Me") {
+                    ScrollValuePicker(num: .constant(50), minNum: .constant(2), maxNum: .constant(650), numType: .constant("kg"), textf: .constant("Current Weight:"))
+                    ScrollValuePicker(num: .constant(150), minNum: .constant(100), maxNum: .constant(300), numType: .constant("cm"), textf: .constant("Current Height:"))
+                }
+            }
         }
         .navigationTitle("Settings") // This adds a title in the NavigationStack
     }
--- FoodVision/View/ScrollValuePicker.swift
+++ FoodVision/View/ScrollValuePicker.swift
@@ -1,18 +1,54 @@
-//
-//  ScrollValuePicker.swift
-//  FoodVision
-//
-//  Created by Tracy Chan on 2025-03-11.
-//
-
 import SwiftUI
 
 struct ScrollValuePicker: View {
+    @Binding var num: Int
+    @Binding var minNum: Int
+    @Binding var maxNum: Int
+    @Binding var numType: String
+    @Binding var textf: String
+    @State var showPicker: Bool = false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Text("\(textf)")
+            if !showPicker {
+                Button(action: {
+                    showPicker.toggle() // Show the picker on button click
+                }) {
+                    Text("\(num) \(numType)")
+                        .padding()
+                        .background(Color.customLightGray)
+                        .foregroundStyle(Color.blackInLight)
+                        .cornerRadius(10)
+                        .onChange(of: num) {
+                            num = 100
+                        }
+                }
+                .padding()
+                
+            } else {
+                HStack{
+                    Picker("Select Number", selection: $num) {
+                        ForEach(minNum...maxNum, id: \.self) { weight in
+                            Text("\(weight) \(numType)").tag(weight)
+                        }
+                    }
+                    .pickerStyle(WheelPickerStyle())
+                    .frame(height: 100)
+                    .background(Color.customLightGray)
+                    .transition(.move(edge: .top).combined(with: .opacity)) // Transition for picker appearance
+                    .onChange(of: num, initial: false) {
+                        num = num
+                    }
+                    Button("Confirm") {
+                        showPicker.toggle()
+                    }
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    ScrollValuePicker()
+    ScrollValuePicker(num: .constant(50), minNum: .constant(2), maxNum: .constant(650), numType: .constant("kg"), textf: .constant("Current Weight:"))
 }
