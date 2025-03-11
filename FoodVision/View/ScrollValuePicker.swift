276759ef7706efd7c315df0bbd5f88c8651e2a8f
Tracy Chan <tracysychan@outlook.com>
2025-03-12 00:21:55 +0800

    feat: scroll value picker

---
+++ FoodVision/View/ScrollValuePicker.swift
@@ -0,0 +1,18 @@
+//
+//  ScrollValuePicker.swift
+//  FoodVision
+//
+//  Created by Tracy Chan on 2025-03-11.
+//
+
+import SwiftUI
+
+struct ScrollValuePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrollValuePicker()
+}
