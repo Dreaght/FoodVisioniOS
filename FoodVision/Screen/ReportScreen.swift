3e70ad8570b0f15df3400ff052b2a91f7dc2e9cf
Tracy Chan <tracysychan@outlook.com>
2025-03-27 00:55:54 +0800

    UI for report screen (date picking)

--- FoodVision/Screen/ReportScreen.swift
+++ FoodVision/Screen/ReportScreen.swift
@@ -1,18 +1,61 @@
 import SwiftUI
 
 struct ReportScreen: View {
+    @State private var startDate = Date()
+    @State private var endDate = Date()
+    private let calendar = Calendar.current
+    
     var body: some View {
+        let dateRange: ClosedRange<Date> = {
+            let startComponents = DateComponents(year: 2025, month: 1, day: 1)
+            let startDate = calendar.date(from: startComponents)!
+            let endDate = Date() // Today's date
+            return startDate...endDate
+        }()
+        
         VStack {
-            Text("ReportScreen")
-                .font(.title)
+            Text("Select the start and end date to generate report for:")
+                .font(.subheadline)
+                .multilineTextAlignment(.leading)
+            Text("Choose up to at most a week")
+                .multilineTextAlignment(.leading)
                 .padding()
+            
+            DatePicker("Pick the start date:",
+                       selection: $startDate,
+                       in: dateRange,
+                       displayedComponents: [.date])
+            .padding()
+            
+            let maxEndDate = min(calendar.date(byAdding: .day, value: 6, to: startDate) ?? Date(), Date())
+            
+            DatePicker("Pick the end date:",
+                       selection: $endDate,
+                       in: startDate...maxEndDate,
+                       displayedComponents: [.date])
+            .padding()
+            Button(action: {
+                            generateReport(startDate: startDate, endDate: endDate)
+                        }) {
+                            Text("Generate Report")
+                                .font(.headline)
+                                .padding()
+                                .frame(maxWidth: .infinity)
+                                .background(.customLightBlue)
+                                .foregroundColor(.customLightGray)
+                                .cornerRadius(8)
+                        }
+                        .padding()
+            Spacer()
         }
-        .navigationTitle("ReportScreen") // This adds a title in the NavigationStack
+        .padding()
+        .navigationTitle("Report Generation")
     }
-}
-
-#Preview {
-    ReportScreen()
+    
+    private func generateReport(startDate: Date, endDate: Date) {
+            // Implement your report generation logic here
+            print("Generating report from \(startDate) to \(endDate)")
+        }
 }
 
 #Preview {
